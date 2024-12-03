358d86bb4fc8b6783d3bfeaafa817c6cc0d7572b
Your Name <you@example.com>
2024-12-03 22:31:10 +0530

    added python code

--- Python/Emails_with_python/mail.py
+++ Python/Emails_with_python/mail.py
@@ -22,7 +22,7 @@ em.set_content(body)
 context = ssl.create_default_context()
 
 try:
-    with smtplib.SMTP_SSL('smtp.gmail.com', 587, context=context) as smtp:
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
         smtp.login(email_sender, email_password)
         smtp.sendmail(email_sender, email_receiver, em.as_string())
         print("Email sent successfully!")  # Success message
