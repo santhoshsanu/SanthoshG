d9674b28bb6b28a16084c61cb26187798952f9db
santhoshsanu <santhoshgullapudi2710@gmail.com>
2025-03-24 12:56:11 +0530

    added

--- Terraform/azure/practice/main.tf
+++ Terraform/azure/practice/main.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
   features {}
 
-  subscription_id = "6a6a9c10-f82a-4e15-bf31-cd5b20019403"
-  tenant_id       = "b5c45fbc-1a54-4e8c-b402-48f9f2f03237"
+  subscription_id = ""
+  tenant_id       = ""
 }
 
 resource "azurerm_resource_group" "main" {
