c1e830e4a1dce461558a51fac1a86548988e5a1b
Your Name <you@example.com>
2025-03-25 22:16:18 +0530

    added

--- Terraform/azure/practice/main.tf
+++ Terraform/azure/practice/main.tf
@@ -7,5 +7,20 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "main" {
     name = "example"
-    location = "West Europe"
+    location = "East US"
+}
+
+resource "azurerm_virtual_network" "primary" {
+  name = "primary-network"
+  location = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space = ["10.0.0.0/16"]
+}
+
+
+resource "azurerm_subnet" "web" {
+  name = "web-subnet"
+  resource_group_name = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.primary.name
+  address_prefixes = ["10.0.0.1/24"]
 }
