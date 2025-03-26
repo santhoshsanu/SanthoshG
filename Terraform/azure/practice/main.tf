6c877563e198d1e551e0141d4ebfa301bda080a2
santhoshsanu <santhoshgullapudi2710@gmail.com>
2025-03-26 13:05:27 +0530

    added

--- Terraform/azure/practice/main.tf
+++ Terraform/azure/practice/main.tf
@@ -2,11 +2,10 @@ provider "azurerm" {
   features {}
 
   subscription_id = ""
-  tenant_id       = ""
 }
 
 resource "azurerm_resource_group" "main" {
-    name = "example"
+    name = "example-tf"
     location = "East US"
 }
 
@@ -15,6 +14,8 @@ resource "azurerm_virtual_network" "primary" {
   location = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   address_space = ["10.0.0.0/16"]
+
+  depends_on = [ azurerm_resource_group.main ]
 }
 
 
@@ -22,5 +23,16 @@ resource "azurerm_subnet" "web" {
   name = "web-subnet"
   resource_group_name = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.primary.name
-  address_prefixes = ["10.0.0.1/24"]
+  address_prefixes = ["10.0.1.0/24"]
+
+  depends_on = [ azurerm_virtual_network.primary ]
+}
+
+resource "azurerm_subnet" "data" {
+  name = "data-subnet"
+  resource_group_name = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.primary.name
+  address_prefixes = ["10.0.2.0/24"]
+
+  depends_on = [ azurerm_virtual_network.primary ]
 }
